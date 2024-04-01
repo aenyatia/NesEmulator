ec422a4c244aee12025e51a08fd00af5cf55750d
Aenyatia Nirvalen <aenyatia@gmail.com>
2024-04-01 19:08:24 +0200

    Add ram.

---
+++ NesEmulator.Core/CpuModule/Ram.cs
@@ -0,0 +1,17 @@
+﻿namespace NesEmulator.Core.CpuModule;
+
+public class Ram : IMemory
+{
+    private readonly byte[] _memory = new byte[0x10000];
+
+    public uint Read(uint address)
+        => _memory[address];
+
+    public void Write(uint address, uint data)
+        => _memory[address] = (byte)data;
+
+    public void Load(byte[] program, uint index)
+    {
+        Array.Copy(program, 0, _memory, index, program.Length);
+    }
+}
