9eaebcec53a86cd477921ba4845e40a0c3039887
Aenyatia <aenyatia@gmail.com>
2024-04-12 19:55:47 +0200

    Add cpu ram and ppu ram.

---
+++ NesEmulator.Core/CpuRam.cs
@@ -0,0 +1,32 @@
+﻿namespace NesEmulator.Core;
+
+public class CpuRam
+{
+    private readonly byte[] _memory = new byte[2048];
+
+    public byte Read(ushort address)
+    {
+        // mirroring 0000 0111 1111 1111 - 0x07FF
+        // 0x0000 - 0x07FF
+        // 0x0800 - 0x0FFF
+        // 0x1000 - 0x17FF
+        // 0x1800 - 0x1FFF
+
+        address &= 0x07FF;
+
+        return _memory[address];
+    }
+
+    public void Write(ushort address, byte data)
+    {
+        // mirroring 0000 0111 1111 1111 - 0x07FF
+        // 0x0000 - 0x07FF
+        // 0x0800 - 0x0FFF
+        // 0x1000 - 0x17FF
+        // 0x1800 - 0x1FFF
+
+        address &= 0x07FF;
+
+        _memory[address] = data;
+    }
+}
---
+++ NesEmulator.Core/PpuRam.cs
@@ -0,0 +1,16 @@
+﻿namespace NesEmulator.Core;
+
+public class PpuRam
+{
+    private readonly byte[] _memory = new byte[2048];
+
+    public byte Read(ushort address)
+    {
+        return _memory[address];
+    }
+
+    public void Write(ushort address, byte data)
+    {
+        _memory[address] = data;
+    }
+}
