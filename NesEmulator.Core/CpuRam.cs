9ebff88e2df4392f0ed20d9f156ef353a6aaa4f5
Aenyatia <aenyatia@gmail.com>
2024-04-14 20:33:19 +0200

    Add ram mirroring.

--- NesEmulator.Core/CpuRam.cs
+++ NesEmulator.Core/CpuRam.cs
@@ -6,11 +6,10 @@ public class CpuRam
 
     public byte Read(ushort address)
     {
-        // mirroring 0000 0111 1111 1111 - 0x07FF
-        // 0x0000 - 0x07FF
-        // 0x0800 - 0x0FFF
-        // 0x1000 - 0x17FF
-        // 0x1800 - 0x1FFF
+        if (address >= 0x2000)
+            throw new Exception("address out of range");
+
+        address = MirrorRamAddress(address);
 
         address &= 0x07FF;
 
@@ -19,14 +18,22 @@ public class CpuRam
 
     public void Write(ushort address, byte data)
     {
+        if (address >= 0x2000)
+            throw new Exception("address out of range");
+
+        address = MirrorRamAddress(address);
+
+        _memory[address] = data;
+    }
+
+    private static ushort MirrorRamAddress(ushort address)
+    {
         // mirroring 0000 0111 1111 1111 - 0x07FF
         // 0x0000 - 0x07FF
         // 0x0800 - 0x0FFF
         // 0x1000 - 0x17FF
         // 0x1800 - 0x1FFF
 
-        address &= 0x07FF;
-
-        _memory[address] = data;
+        return (ushort)(address & 0x07FF);
     }
 }
--- NesEmulator.Core/PpuRam.cs
+++ NesEmulator.Core/PpuRam.cs
@@ -1,16 +1,69 @@
-﻿namespace NesEmulator.Core;
+﻿using NesEmulator.Core.CartridgeModule;
 
-public class PpuRam
+namespace NesEmulator.Core;
+
+public class PpuRam(Mirroring mirroring)
 {
     private readonly byte[] _memory = new byte[2048];
 
     public byte Read(ushort address)
     {
+        address = MirrorVRamAddress(address);
+
         return _memory[address];
     }
 
     public void Write(ushort address, byte data)
     {
+        address = MirrorVRamAddress(address);
+
         _memory[address] = data;
+    }
+
+    private ushort MirrorVRamAddress(ushort address)
+    {
+        // address from 0x2000 to 0x2FFF 4kB
+        address -= 0x2000; // 0x0000 - 0x0FFF
+
+        var nameTableIndex = address / 0x400;
+
+        // Vertical mirroring: $2000 equals $2800 and $2400 equals $2C00
+        // Horizontal mirroring: $2000 equals $2400 and $2800 equals $2C00
+        if (mirroring == Mirroring.Horizontal)
+        {
+            // inx == 0 nothing to do
+
+            if (nameTableIndex == 1)
+            {
+                address -= 0x0400;
+            }
+
+            if (nameTableIndex == 2)
+            {
+                address -= 0x0400;
+            }
+
+            if (nameTableIndex == 3)
+            {
+                address -= 0x0800;
+            }
+        }
+
+        if (mirroring == Mirroring.Vertical)
+        {
+            // inx == 0 or 1 nothing to do
+
+            if (address == 2)
+            {
+                address -= 0x800;
+            }
+
+            if (address == 3)
+            {
+                address -= 0x800;
+            }
+        }
+
+        return address;
     }
 }
